9f07482d809debb92bc025ef060f276bd1a38cf3
Rocco Bowling <rocco@chimerasw.com>
2017-07-12 17:04:54 -0400

    WIP: getting carthage to build

---
+++ libextobjc/libextobjc.h
@@ -0,0 +1,19 @@
+//
+//  libextobjc.h
+//  libextobjc
+//
+//  Created by Rocco Bowling on 7/12/17.
+//  Copyright © 2017 Rocco Bowling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for libextobjc.
+FOUNDATION_EXPORT double libextobjcVersionNumber;
+
+//! Project version string for libextobjc.
+FOUNDATION_EXPORT const unsigned char libextobjcVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libextobjc/PublicHeader.h>
+
+
